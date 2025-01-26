bed2695231f04ed286cb9f58deaa8976f1636d4e
korsakPz <76098258+korsakPz@users.noreply.github.com>
2025-01-26 22:26:53 +0300

    Лекция 2.1. Исключения

--- sandbox/src/main/java/Hello.java
+++ sandbox/src/main/java/Hello.java
@@ -1,14 +1,29 @@
-import java.io.File;
-
 public class Hello {
     public static void main(String[] args)
     {
-        System.out.println("Hello world");
+        try {
+            int z = calculate();
+            System.out.println(z);
+            System.out.println("Hello world");
 
-        var configFile =  new File("sandbox/build.gradle");
-        System.out.println(configFile.getAbsolutePath());
-        System.out.println(configFile.exists());
+        } catch (ArithmeticException exception) {
+
+            exception.printStackTrace();
+        }
 
 
+
+    }
+
+    private static int calculate() {
+        var x = 1;
+        var y = 1;
+        var z = divide(x, y);
+        return z;
+    }
+
+    private static int divide(int x, int y) {
+        var z = x / y;
+        return z;
     }
 }
