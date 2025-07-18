da8c981776214e7074a80164cf7373c13754492f
korsakPz <76098258+korsakPz@users.noreply.github.com>
2025-07-18 22:19:17 +0300

    Add addressbok modul

---
+++ addressbook_web_tests/src/test/java/GroupCreationTests.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+public class GroupCreationTests extends TestBase {
+
+
+    @Test
+    public void canCreateGroup() {
+
+        openGroupsPage();
+        createGroup("group name", "group header", "group footer");
+
+    }
+
+    @Test
+    public void canCreateGroupWithEmptyName() {
+
+        openGroupsPage();
+        createGroup("", "", "");
+
+    }
+}
---
+++ addressbook_web_tests/src/test/java/GroupRemovalTests.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+public class GroupRemovalTests extends TestBase {
+
+
+    @Test
+    public void canRemoveGroup() {
+
+        openGroupsPage();
+        if (!isGroupPresent()) {
+
+            createGroup("", "", "");
+
+        }
+
+
+        removeGroup();
+
+    }
+
+}
