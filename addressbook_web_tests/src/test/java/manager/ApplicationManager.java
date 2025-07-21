1e3ffd76b37569516184d1adf8d4c48aaa82720c
korsakPz <76098258+korsakPz@users.noreply.github.com>
2025-07-21 17:53:53 +0300

    Новые пакеты

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -1,29 +1,18 @@
+package manager;
+
 import model.GroupData;
-import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class TestBase {
-
-
+public class ApplicationManager {
     protected static WebDriver driver;
 
-    protected static void removeGroup() {
-        driver.findElement(By.name("selected[]")).click();
-        driver.findElement(By.name("delete")).click();
-        driver.findElement(By.linkText("group page")).click();
-    }
-
-    @BeforeEach
-    public void setUp() {
-
+    public void init() {
         if (driver == null) {
-
             driver = new ChromeDriver();
             Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
-
             driver.get("http://localhost/addressbook/");  //Open and login
             driver.findElement(By.name("user")).click();
             driver.findElement(By.name("user")).sendKeys("admin");
@@ -32,11 +21,9 @@ public class TestBase {
             driver.findElement(By.xpath("(//input[@value=\'Login\'])[1]")).click();
 
         }
-
-
     }
 
-    protected boolean isElementPresent(By locator) {
+    public boolean isElementPresent(By locator) {
         try {
             driver.findElement(locator);
             return true;
@@ -47,7 +34,7 @@ public class TestBase {
 
     }
 
-    protected void createGroup(GroupData group) {
+    public void createGroup(GroupData group) {
         driver.findElement(By.name("new")).click();
         driver.findElement(By.name("group_name")).click();
         driver.findElement(By.name("group_name")).sendKeys(group.name());
@@ -59,13 +46,19 @@ public class TestBase {
         driver.findElement(By.linkText("group page")).click();
     }
 
-    protected void openGroupsPage() {
+    public void openGroupsPage() {
         if (!isElementPresent(By.name("new"))) {
             driver.findElement(By.linkText("groups")).click();
         }
     }
 
-    protected boolean isGroupPresent() {
+    public boolean isGroupPresent() {
         return isElementPresent(By.name("selected[]"));
+    }
+
+    public   void removeGroup() {
+        driver.findElement(By.name("selected[]")).click();
+        driver.findElement(By.name("delete")).click();
+        driver.findElement(By.linkText("group page")).click();
     }
 }
--- addressbook_web_tests/src/test/java/tests/GroupRemovalTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupRemovalTests.java
@@ -1,3 +1,5 @@
+package tests;
+
 import model.GroupData;
 import org.junit.jupiter.api.Test;
 
@@ -7,15 +9,12 @@ public class GroupRemovalTests extends TestBase {
     @Test
     public void canRemoveGroup() {
 
-        openGroupsPage();
-        if (!isGroupPresent()) {
-
-            createGroup(new GroupData("", "", ""));
+        app.openGroupsPage();
+        if (!app.isGroupPresent()) {
+            app.createGroup(new GroupData("", "", ""));
 
         }
-
-
-        removeGroup();
+        app.removeGroup();
 
     }
 
