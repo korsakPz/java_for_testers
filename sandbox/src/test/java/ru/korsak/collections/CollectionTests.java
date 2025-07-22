3646368d8474e4d2bbf47a7936335f5c1508d44b
korsakPz <76098258+korsakPz@users.noreply.github.com>
2025-07-22 21:49:47 +0300

    Массивы

--- sandbox/src/test/java/ru/korsak/collections/CollectionTests.java
+++ sandbox/src/test/java/ru/korsak/collections/CollectionTests.java
@@ -3,16 +3,34 @@ package ru.korsak.collections;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CollectionTests {
 
     @Test
     void arrayTests(){
-        //var array = new String[]{"a","b","c"};
-        var array = new String[3];
+        var array = new String[]{"a","b","c"};
+        Assertions.assertEquals(3, array.length);
+
+        array[0] = "a";
         Assertions.assertEquals("a",array[0]);
 
         array[0] = "d";
         Assertions.assertEquals("d",array[0]);
+
+    }
+
+    @Test
+    void listTests() {
+        //var list = List.of("x", "w", "z");
+        var list = new ArrayList<>(List.of("x", "w", "z"));
+
+        Assertions.assertEquals(3, list.size());
+        Assertions.assertEquals("x", list.get(0));
+
+        list.set(0, "z");
+        Assertions.assertEquals("z", list.get(0));
 
     }
 }
