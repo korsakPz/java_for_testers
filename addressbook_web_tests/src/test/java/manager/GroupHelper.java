58bdfe8b8eccde71c41bfc1a11343e92963eaaf0
korsakPz <76098258+korsakPz@users.noreply.github.com>
2025-07-22 16:26:33 +0300

    Добавление ожидания Duration

--- addressbook_web_tests/src/test/java/manager/GroupHelper.java
+++ addressbook_web_tests/src/test/java/manager/GroupHelper.java
@@ -2,6 +2,10 @@ package manager;
 
 import model.GroupData;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class GroupHelper extends HelperBase{
 
@@ -70,6 +74,8 @@ public class GroupHelper extends HelperBase{
 
 
     private void returnToGroupsPage() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.linkText("group page")));
         click(By.linkText("group page"));
     }
 
--- addressbook_web_tests/src/test/java/manager/LoginHelper.java
+++ addressbook_web_tests/src/test/java/manager/LoginHelper.java
@@ -10,14 +10,10 @@ public class LoginHelper extends HelperBase{
     void login(String user, String password) {
 
         click(By.name("user"));
-        //manager.driver.findElement(By.name("user")).click();
         type(By.name("user"), user);
-        //manager.driver.findElement(By.name("user")).sendKeys(user);
         click(By.name("pass"));
-        //manager.driver.findElement(By.name("pass")).click();
         type(By.name("pass"), password);
-        //manager.driver.findElement(By.name("pass")).sendKeys(password);
         click(By.xpath("(//input[@value=\'Login\'])[1]"));
-        //manager.driver.findElement(By.xpath("(//input[@value=\'Login\'])[1]")).click();
+
     }
 }
