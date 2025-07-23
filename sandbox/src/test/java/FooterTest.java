d075ed4f08e02644cd0c740b88366806df176094
korsakPz <76098258+korsakPz@users.noreply.github.com>
2025-07-23 12:06:23 +0300

    Only_test 2

--- sandbox/src/test/java/FooterTest.java
+++ sandbox/src/test/java/FooterTest.java
@@ -1,3 +1,16 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
 public class FooterTest {
 
     private WebDriver driver;
